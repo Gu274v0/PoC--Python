76223791fe307d3c6edfc68e90d6458129ac3049
Gustavo Silva <gustavosteventos@hotmail.com>
2022-06-20 17:04:40 +0100

    enhancement main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from src.router import api_router
 
-app = FastAPI(title="Python - PoC")
-
+title = "Python - PoC"
+app = FastAPI(title=title)
 app.include_router(api_router)
+
+
+@app.get("/", name="Home", description="Home Page")
+def home():
+    return {
+        "title": 'Welcome to ' + title,
+        "routes": [{"path": route.path, "name": route.name, "description": route.description if hasattr(route, 'description') else "Withou descriptioon", "type": None}
+                   for route in app.routes]
+    }
